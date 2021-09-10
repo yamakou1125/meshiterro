e2c11d7cb64d9bb20f8b1d9f11cb943a0c634ac0
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-09-10 08:59:06 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '86039f5446456e6333f6b04756866137d2e6736ec3e2cfea43231fb1c92383c668c567239b17e7cdda18cac99d67b563ee3d8d4a4ba99577632e830d69f1436e'
